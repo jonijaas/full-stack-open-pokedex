40fe7ca08b2981186d0efd92b765a8d7ac946d8c
jonijaas <joni.jaaskelainen2@metropolia.fi>
2022-08-02 14:48:12 +0300

    11.9 first test fixed

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { // eslint-disable-line
-      // implement node event listeners here
-    },
+    'video': false
   },
 })
